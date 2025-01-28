d4a69e851d8bb3e776ce05d4150e7c29d347c274
David Johnson <djohnson@krum.io>
2025-01-28 14:18:35 -0500

    fix: Attempt to correct build error:

--- pkg/cloud-casa-extension/vue.config.js
+++ pkg/cloud-casa-extension/vue.config.js
@@ -1,3 +1,1 @@
-module.exports = require('./.shell/pkg/vue.config')(__dirname, {
-  proxies: { 'rhn/manager': configHelper.proxyOpts('api.cloudcasa.io') }
-});
+module.exports = require('./.shell/pkg/vue.config')(__dirname);
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const config = require('@rancher/shell/vue.config'); // eslint-disable-line @typescript-eslint/no-var-requires
+const configHelper = require('@rancher/shell/vue-config-helper.js');
 
 module.exports = config(__dirname, {
   excludes: [],
-  // excludes: ['fleet', 'example']
+  proxies: { 'rhn/manager': configHelper.proxyOpts('api.cloudcasa.io') }
 });
