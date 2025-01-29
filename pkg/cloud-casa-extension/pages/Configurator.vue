f084ab820c9293b01e490325c459d7bc0d89f9cd
David Johnson <djohnson@krum.io>
2025-01-29 09:15:03 -0500

    feat: A bit more work on centralizing styling, and starting work on input:

---
+++ pkg/cloud-casa-extension/pages/Configurator.vue
@@ -0,0 +1,61 @@
+<script>
+import { defineComponent } from 'vue';
+import SimpleBox from '@shell/components/SimpleBox';
+import LabeledInput from '@shell/components/form/LabeledInput';
+
+export default defineComponent({
+  layout: 'plain', /*This is going to be deprecated in the future, when it breaks
+  try looking at change log or reaching out to Alexandre Alves. Reference this
+  GitHub issue: https://github.com/rancher/dashboard/issues/12980*/
+  components: {
+    SimpleBox,
+    LabeledInput
+  },
+  data() {
+    return {
+      ccApiKey: ''
+    }
+  }
+})
+</script>
+<template>
+  <div class="flex-box">
+    <div class="flex-item">
+      <SimpleBox class="simplebox-centering">
+        <img src="https://cloudcasa.io/assets/logo-white-1.png" />
+        <h1>Welcome to the CloudCasa Extension!</h1>
+        <h3>In order to get started you will need a CloudCasa API Key. To set 
+          that up follow 
+          <a href="https://docs.cloudcasa.io/help/apikeys.html">this guide</a>.
+        </h3>
+      </SimpleBox>
+    </div>
+    <div class="flex-item">
+      <SimpleBox class="simplebox-centering">
+         <LabeledInput type="text" v-model:value="ccApiKey">
+            <template #label>CloudCasa API Key</template>
+        </LabeledInput>
+      </SimpleBox>
+    </div>
+  </div>
+</template>
+<style>
+  .flex-box{
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+  }
+
+  .flex-item{
+    flex: 1;
+  }
+
+  .simplebox-centering{
+    text-align: center;
+    height: 93vh;
+    margin: 1rem;
+    display: flex; 
+    align-items: center;
+    justify-content: center;
+  }
+</style>
--- pkg/cloud-casa-extension/routing/extension-routing.js
+++ pkg/cloud-casa-extension/routing/extension-routing.js
@@ -1,14 +1,25 @@
 import Dashboard from '../pages/index.vue';
+import Configurator from '../pages/Configurator.vue';
 import DetailedCluster from '../pages/DetailedCluster.vue';
 
 const BLANK_CLUSTER = '_';
 const YOUR_PRODUCT_NAME = 'cloud-casa';
 
 const routes = [
-  {
+  /*{
     name: `${YOUR_PRODUCT_NAME}`,
     path: `/${YOUR_PRODUCT_NAME}`,
     component: Dashboard,
+    meta: {
+      product: YOUR_PRODUCT_NAME,
+      pkg: YOUR_PRODUCT_NAME,
+      cluster: BLANK_CLUSTER,
+    },
+  },*/
+  {
+    name: `${YOUR_PRODUCT_NAME}`,
+    path: `/${YOUR_PRODUCT_NAME}`,
+    component: Configurator,
     meta: {
       product: YOUR_PRODUCT_NAME,
       pkg: YOUR_PRODUCT_NAME,
