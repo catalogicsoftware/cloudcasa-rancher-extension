1d99e9f5cd416390f9fa172f3b3277c1bfc02f8d
David Johnson <djohnson@krum.io>
2025-01-29 10:19:32 -0500

    chore: add annotations to build:

--- pkg/cloud-casa-extension/pages/index.vue
+++ pkg/cloud-casa-extension/pages/index.vue
@@ -33,7 +33,7 @@ export default defineComponent({
       );
 
       const setting = whitelist.find(s => s.value === url);
-      
+      console.log(setting);      
       if (setting != undefined) {
         return true;
       }
--- pkg/cloud-casa-extension/vue.config.js
+++ pkg/cloud-casa-extension/vue.config.js
@@ -1,1 +1,6 @@
-module.exports = require('./.shell/pkg/vue.config')(__dirname);
+const configHelper = require('./shell/vue-config-helper.js');
+
+module.exports = require('./.shell/pkg/vue.config')(__dirname, {
+  excludes: [],
+  proxies: { 'rhn/manager': configHelper.proxyOpts('api.cloudcasa.io') }
+});
