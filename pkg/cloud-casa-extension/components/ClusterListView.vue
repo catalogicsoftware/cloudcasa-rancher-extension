3249a0b190415fa8923d3f2ea8991b5facbff799
David Johnson <djohnson@krum.io>
2025-03-02 21:15:11 -0500

    chore: renamed InstallButton.vue to ClusterActions.vue since uninstall is now part of that component:

--- pkg/cloud-casa-extension/components/ClusterListView.vue
+++ pkg/cloud-casa-extension/components/ClusterListView.vue
@@ -1,7 +1,7 @@
 <script>
 import { defineComponent } from 'vue';
 import DashboardButton from './DashboardButton.vue';
-import InstallButton from './cluster_list_view/InstallButton.vue';
+import ClusterActions from './cluster_list_view/ClusterActions.vue';
 import ClusterState from './cluster_list_view/ClusterState.vue';
 import { CRD_NAME, CLOUDCASA_URL, PRODUCT_NAME } from './../types/types.js';
 
@@ -24,7 +24,7 @@ export default defineComponent({
     SortableTable,
     DashboardButton,
     BadgeState,
-    InstallButton,
+    ClusterActions,
     ClusterState,
     FontAwesomeIcon,
   },
@@ -287,7 +287,7 @@ export default defineComponent({
               </div>
             </template>
             <template #cell:install="{ row }">
-              <InstallButton 
+              <ClusterActions 
                 :row="row" 
                 @install-state-func="setInstallState"
                 :cloudCasaApiKey="cloudCasaApiKey" 
